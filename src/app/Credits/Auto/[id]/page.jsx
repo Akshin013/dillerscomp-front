11f7bb14215dd788ea7ed3a8359dd8f541b91372
Akshin013 <akshingadimov@gmail.com>
2026-01-10 12:21:42 +0400

    Update page.jsx

--- src/app/Credits/Auto/[id]/page.jsx
+++ src/app/Credits/Auto/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
-import Breadcrumbs from "@/components/Breadcrumbs";
+import Breadcrumbs from "@/Components/Breadcrumbs";
 
 const CREDIT_SUBTYPE_MAP = {
   CONSUMER_LOAN: "Потребительский кредит",
