3c0a9322a0fd499e116d2695019bca5e94e512a6
Akshin013 <akshingadimov@gmail.com>
2026-01-10 12:22:16 +0400

    Update page.jsx

--- src/app/Credits/Consumer/[id]/page.jsx
+++ src/app/Credits/Consumer/[id]/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import Breadcrumbs from "@/components/Breadcrumbs";
+import Breadcrumbs from "@/Components/Breadcrumbs";
 
 export default function ConsumerCreditDetailPage() {
   const { id } = useParams();
